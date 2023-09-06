be4c9c173a03e37e1b5d8ec722c32483b422e970
Nguyễn Ngọc Thông <thongnguyenngoc3738@gmail.com>
2023-09-06 16:13:55 +0700

    add unit test for order service, appointment repo

--- src/main/java/com/hairsalon/respository/ServiceHairRepository.java
+++ src/main/java/com/hairsalon/respository/ServiceHairRepository.java
@@ -5,9 +5,12 @@ import com.hairsalon.model.HairServiceModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+
+@Repository
 public interface ServiceHairRepository extends JpaRepository<ServiceHair, Integer> {
     @Query(value = "SELECT S FROM ServiceHair S WHERE S.serviceName LIKE CONCAT('%', :serviceName, '%')")
     List<ServiceHair> findByPartialServiceName(@Param("serviceName") String serviceName);
